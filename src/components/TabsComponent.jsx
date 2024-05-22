ec0005f36e26812ec7a68bd69b4768e25d179dc5
sai-kiran77 <manchikantisaikiran777@gmail.com>
2024-05-22 19:43:07 +0530

    Add tabs bug fix, handling uuid, one tab is default/mandatory

--- src/components/TabsComponent.jsx
+++ src/components/TabsComponent.jsx
@@ -1,6 +1,20 @@
 import { Tabs } from "antd";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import AppEditorComponent from "./AppEditorComponent";
+import { v4 as uuidv4 } from 'uuid';
+
+function EditorWrapper({
+  checked,
+  uuid,
+  setInitialItems
+}) {
+  const editorRef = useRef();
+  setInitialItems((prev) => {
+    prev.find((obj) => obj.key === uuid).editorRef = editorRef;
+    return prev;
+  })
+  return <AppEditorComponent editorRef={editorRef} checked={checked} />
+}
 
 function TabsComponent({
   checked,
@@ -8,44 +22,52 @@ function TabsComponent({
   initialItems = [],
   setActiveKey,
   activeKey,
+  setInitialItems,
+  showNotification
 }) {
-  const [items, setItems] = useState(
-    initialItems.map((obj) => {
-      const editorRef = useRef();
-      obj.children = <AppEditorComponent editorRef={editorRef} checked={checked} />;
-      obj.pageName = pageName;
-      obj.editorRef = editorRef;
-      return obj;
-    })
-  );
   const newTabIndex = useRef(0);
   const onChange = (newActiveKey) => {
     setActiveKey(newActiveKey);
   };
 
+  useEffect(() => {
+    setInitialItems(initialItems.map((obj, i) => {
+      obj.children = <EditorWrapper setInitialItems={setInitialItems} uuid={obj.key} checked={checked} />;
+      obj.pageName = pageName;
+      return obj;
+    }));
+  }, [])
+
   const add = () => {
-    const editorRef = useRef();
-    const newPanes = [...items];
-    const newActiveKey = newPanes.length + 1 + 1;
-    newPanes.push({
-      label: `Tab ${newPanes.length + 1}`,
-      children: <AppEditorComponent editorRef={editorRef} checked={checked} />,
-      editorRef: editorRef,
-      key: newActiveKey,
+    setInitialItems((prevItems) => {
+      const newPanes = [...prevItems];
+      const newActiveKey = uuidv4();
+      newPanes.push({
+        label: `Tab ${newPanes.length + 1}`,
+        children: <EditorWrapper setInitialItems={setInitialItems} uuid={newActiveKey} checked={checked} />,
+        key: newActiveKey,
+      });
+      setActiveKey(newActiveKey);
+      return newPanes;
     });
-    setItems(newPanes);
-    setActiveKey(newActiveKey);
   };
 
+
   const remove = (targetKey) => {
+
+    if (initialItems.length === 1) {
+      showNotification("Removing the last tab is not allowed", 3, "warning");
+      return;
+    }
+
     let newActiveKey = activeKey;
     let lastIndex = -1;
-    items.forEach((item, i) => {
+    initialItems.forEach((item, i) => {
       if (item.key === targetKey) {
         lastIndex = i - 1;
       }
     });
-    const newPanes = items.filter((item) => item.key !== targetKey);
+    const newPanes = initialItems.filter((item) => item.key !== targetKey);
     if (newPanes.length && newActiveKey === targetKey) {
       if (lastIndex >= 0) {
         newActiveKey = newPanes[lastIndex].key;
@@ -53,7 +75,7 @@ function TabsComponent({
         newActiveKey = newPanes[0].key;
       }
     }
-    setItems(newPanes);
+    setInitialItems(newPanes.map((obj, i) => { obj.label = `Tab ${i + 1}`; return obj }));
     setActiveKey(newActiveKey);
   };
 
@@ -72,7 +94,7 @@ function TabsComponent({
       onChange={onChange}
       activeKey={activeKey}
       onEdit={onEdit}
-      items={items}
+      items={initialItems}
     />
   );
 }
