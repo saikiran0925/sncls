dac3af4a37adad5457162a64a99042790546264b
saikiranbade <sai.kiran.bade@nextiva.com>
2024-06-02 01:02:03 +0530

    Implement Diff Editor
    1. Implement tabs feature for Diff Editor
    2. Implement local storage feature for Diff Editor

--- src/components/AppDiffEditorComponent.jsx
+++ src/components/AppDiffEditorComponent.jsx
@@ -1,22 +1,57 @@
 import { DiffEditor } from "@monaco-editor/react";
+import { useEffect, useRef, useState } from "react";
+import ForwardedDiffEditor from "./ForwardedDiffEditor";
 
-function AppDiffEditorComponent() {
-  function handleEditorMount(editor) {
+function AppDiffEditorComponent(props) {
+  const editorRef = props.editorRef;
+  let modifiedEditorContent;
+  let originalEditorContent;
+  let currentEditorState;
+  if (editorRef.current) {
+    currentEditorState = editorRef.current.editorState;
+  }
+
+  console.log("Current editorRef is: ", editorRef.current);
+
+  function handleEditorMount(editor, monaco) {
+    console.log("Editor instance: ", editor);
     const modifiedEditor = editor.getModifiedEditor();
     const originalEditor = editor.getOriginalEditor();
+    originalEditor.setValue(props.editorState.originalEditorContent);
+    modifiedEditor.setValue(props.editorState.modifiedEditorContent);
+    console.log(
+      "Setting the values after mount and the values are: ",
+      props.editorState.modifiedEditorContent,
+      props.editorState.originalEditorContent
+    );
     modifiedEditor.onDidChangeModelContent((_) => {
       console.log(modifiedEditor.getValue());
+      modifiedEditorContent = modifiedEditor.getValue();
+      const editorState = {
+        originalEditorContent,
+        modifiedEditorContent,
+      };
+      currentEditorState = editorState;
+      console.log("Editor State: ", currentEditorState);
+      props.onEditorStateChange(currentEditorState);
     });
     originalEditor.onDidChangeModelContent((_) => {
       console.log(originalEditor.getValue());
+      originalEditorContent = originalEditor.getValue();
+      const editorState = {
+        originalEditorContent,
+        modifiedEditorContent,
+      };
+      currentEditorState = editorState;
+      console.log("Editor State: ", currentEditorState);
+      props.onEditorStateChange(currentEditorState);
     });
   }
 
   return (
-    <DiffEditor
+    <ForwardedDiffEditor
+      ref={editorRef}
       height="90vh"
-      original=""
-      modified=""
       onMount={handleEditorMount}
       options={{
         readOnly: false,
@@ -25,5 +60,5 @@ function AppDiffEditorComponent() {
     />
   );
 }
-
+AppDiffEditorComponent.displayName = "AppDiffEditorComponent";
 export default AppDiffEditorComponent;
